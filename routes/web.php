e4c9211b67abdd570a6655b5d7e7f3a3453f3481
Santiago Fierro <santimarcelofierrod@gmail.com>
2023-07-04 22:36:00 -0500

    login funcionando

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
@@ -19,12 +20,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// ------------Users----------------
+
 Route::resource('users', UserController::class)->except(
     'show',
     'destroy'
-);
+)->middleware('auth');
 
 Route::resource('roles', RoleController::class)->except(
     'show',
     'destroy'
-);
+)->middleware('auth');
+
+// ------------Login----------------
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('auth', [LoginController::class, 'authenticate'])->name('auth');
+
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');

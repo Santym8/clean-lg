80e90806bdb4488b7548ff33a081803ca29cbf33
Franklin Enriquez <flenriquez@uce.edu.ec>
2023-07-06 13:54:26 -0500

    Dashboard

---
+++ resources/views/auth/includes/scripts.blade.php
@@ -0,0 +1,9 @@
+<script src="{{ asset('assets/auth/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/auth/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/auth/plugins/simplebar/simplebar.min.js') }}"></script>
+<script src="https://unpkg.com/hotkeys-js/dist/hotkeys.min.js"></script>
+
+
+<script src="{{ asset('assets/auth/js/mono.js') }}"></script>
+<script src="{{ asset('assets/auth/js/map.js') }}"></script>
+<script src="{{ asset('assets/auth/js/custom.js') }}"></script>

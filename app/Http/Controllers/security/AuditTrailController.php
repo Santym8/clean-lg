f4c791c8eb1d68eff35fd700099caf85fe2ad70f
Santiago Fierro <santimarcelofierrod@gmail.com>
2023-07-06 17:05:44 -0500

    creacion de archivos base

---
+++ app/Http/Controllers/security/AuditTrailController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\security;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class AuditTrailController extends Controller
+{
+    //
+}
---
+++ app/Models/security/AuditTrail.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\security;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AuditTrail extends Model
+{
+    use HasFactory;
+}

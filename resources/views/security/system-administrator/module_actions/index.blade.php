cbb15f1e5a365d0993b676039c2dc066fdf07236
Santiago Fierro <santimarcelofierrod@gmail.com>
2023-07-17 08:13:19 -0500

    estilos acciones de modulos

--- resources/views/security/system-administrator/module_actions/index.blade.php
+++ resources/views/security/system-administrator/module_actions/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <table class="table">
+        <table class="table" id="module-actions-table">
 
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error') }}</div>
@@ -72,3 +72,21 @@
         </table>
     </div>
 @endsection
+
+@push('styles')
+    <link href="//cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+@endpush
+@push('scripts')
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#module-actions-table').DataTable({
+                // Configuración personalizada de DataTables
+            });
+        });
+    </script>
+@endpush

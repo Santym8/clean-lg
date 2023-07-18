1966547691fdf28be58c6f37b16d34133287c84d
Leonardo Flores <blfloresa@gmail.com>
2023-07-18 14:04:51 -0500

    validacion campos inventario

--- database/migrations/2023_07_04_025303_create_products_table.php
+++ database/migrations/2023_07_04_025303_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->String('name');
+            $table->String('name',25)->unique();
             $table->boolean('status') -> default(1);
             $table->timestamps();
 
--- database/migrations/2023_07_04_031104_create_product_warehouses_table.php
+++ database/migrations/2023_07_04_031104_create_product_warehouses_table.php
@@ -14,14 +14,16 @@ return new class extends Migration
         Schema::create('product_warehouses', function (Blueprint $table) {
             $table->id();
             $table->integer('cantidad');
-            $table->boolean('status') -> default(1);
+            $table->boolean('status')->default(1);
 
             $table->unsignedBigInteger('product_id');
-            $table->unsignedBigInteger('warehouse_id');            
+            $table->unsignedBigInteger('warehouse_id');
             $table->foreign('product_id')->references('id')->on('products');
             $table->foreign('warehouse_id')->references('id')->on('warehouses');
 
             $table->timestamps();
+            // Agregar índice único compuesto
+            $table->unique(['product_id', 'warehouse_id']);
         });
     }
 

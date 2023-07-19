fc60b9d26a4fe9ed13af9cefeddb8e48345c06d9
Franklin Enriquez <flenriquez@uce.edu.ec>
2023-07-18 20:55:11 -0500

    graficas de modulos mas visitados y acceso exitoso

--- app/Http/Controllers/audit_trail/AudiGraphicsController.php
+++ app/Http/Controllers/audit_trail/AudiGraphicsController.php
@@ -59,7 +59,38 @@ class AudiGraphicsController extends Controller
 
         $chartDataJson2 = json_encode(array_values($chartData2));
 
+        $results3 = AuditTrail::selectRaw('users.name as user_name, COUNT(*) as count')
+            ->where('type', 'LIKE', '%SUCCESSFUL%')
+            ->join('users', 'audit_trails.user_id', '=', 'users.id')
+            ->groupBy('user_name')
+            ->get();
+
+        $chartData3 = [];
+        foreach ($results3 as $row) {
+            $user = $row->user_name;
+            $visits = intval($row->count);
+
+            $chartData3[] = ['name' => $user, 'y' => $visits];
+        }
+
+        $chartDataJson3 = json_encode($chartData3);
+
+        // Obtener todos los módulos más visitados con la cantidad de visitas
+        $allResults = DB::table('audit_trails')
+            ->join('modules', 'audit_trails.type', 'LIKE', DB::raw("CONCAT('%', modules.name, '/%')"))
+            ->select('modules.name as module', DB::raw('COUNT(*) as count'))
+            ->groupBy('module')
+            ->orderByDesc('count')
+            ->get();
+
+        $allChartData = [];
+        foreach ($allResults as $row) {
+            $allChartData[] = ['name' => $row->module, 'y' => intval($row->count)];
+        }
+
+        $allChartDataJson = json_encode($allChartData);
+
         // Retorna la vista con los datos para la gráfica de pastel
-        return view('audit_trail.graphics', compact('chartDataJson', 'chartDataJson2', 'chartData2'));
+        return view('audit_trail.graphics', compact('chartDataJson', 'chartDataJson2', 'chartData2', 'chartDataJson3', 'chartData3', 'allChartDataJson', 'allChartData'));
     }
 }
--- resources/views/audit_trail/graphics.blade.php
+++ resources/views/audit_trail/graphics.blade.php
@@ -50,6 +50,62 @@
             series: <?php echo $chartDataJson2; ?> // Sin las llaves de Blade
         });
     </script>
+    <div id="userAccessColumnChartContainer"></div>
+    <script>
+        Highcharts.chart('userAccessColumnChartContainer', {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Accesos Exitosos por Usuario'
+            },
+            xAxis: {
+                categories: <?php echo json_encode(array_column($chartData3, 'name')); ?>
+            },
+            yAxis: {
+                title: {
+                    text: 'Número de Accesos Exitosos'
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle'
+            },
+            series: [{
+                name: 'Successful Accesses',
+                data: <?php echo $chartDataJson3; ?>
+            }]
+        });
+    </script>
+    <div id="basicLineChartContainer"></div>
+    <script>
+        // ... existing code ...
+
+        Highcharts.chart('basicLineChartContainer', {
+            chart: {
+                type: 'line'
+            },
+            title: {
+                text: 'Módulos más visitados'
+            },
+            xAxis: {
+                title: {
+                    text: 'Módulos'
+                },
+                categories: <?php echo json_encode(array_column($allChartData, 'name')); ?>
+            },
+            yAxis: {
+                title: {
+                    text: 'Cantidad de visitas'
+                }
+            },
+            series: [{
+                name: 'Cantidad',
+                data: <?php echo $allChartDataJson; ?> // Sin las llaves de Blade
+            }]
+        });
+    </script>
 @endsection
 
 @push('styles')
